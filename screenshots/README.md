803f3c8d6a919925728b06d7265865b5210e35d3
thaersaidi <131191852+thaersaidi@users.noreply.github.com>
2023-09-21 22:05:33 +0200

    Added a second test without POM for reference

--- screenshots/README.md
+++
@@ -1,1 +0,0 @@
-### Screenshots for the different steps of the test
--- tests/selenium/test_selenium.py
+++ tests/selenium/test_selenium.py
@@ -1,10 +1,16 @@
-from src.common.selenium.pages.login_page import LoginPage
-from src.config import config
+from selenium.webdriver.common.by import By
 
 
 # Configuration values for the tests
-page_url = config["url"]
+page_url = "https://practicetestautomation.com/practice-test-login/"
 home_page_url = "https://practicetestautomation.com/logged-in-successfully/"
+username = "student"
+password = "Password123"
+login_page_locators = {
+    "username": (By.ID, "username"),
+    "password": (By.ID, "password"),
+    "login_button": (By.ID, "submit")
+}
 
 
 def test_login(driver):
@@ -12,9 +18,15 @@ def test_login(driver):
     Test the login functionality.
     """
     print("Testing login functionality...")
-    login_page = LoginPage(driver)
+    driver.get(page_url)
+    username_locator = login_page_locators["username"]
+    password_locator = login_page_locators["password"]
+    login_button_locator = login_page_locators["login_button"]
     driver.save_screenshot("screenshots/step1.png")
-    login_page.login()
+    driver.find_element(*username_locator).send_keys(username)
+    driver.find_element(*password_locator).send_keys(password)
     driver.save_screenshot("screenshots/step2.png")
+    driver.find_element(*login_button_locator).click()
+    driver.save_screenshot("screenshots/step3.png")
     assert driver.current_url == home_page_url
     print("Login test passed!")
