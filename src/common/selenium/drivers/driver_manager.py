acf96531c9161fa08388fcb152074d3c155f3b18
thaersaidi <131191852+thaersaidi@users.noreply.github.com>
2023-09-21 14:39:35 +0200

    Added options to remote driver

--- src/common/selenium/drivers/driver_manager.py
+++ src/common/selenium/drivers/driver_manager.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
 
 
 class DriverManager:
 
+    options = FirefoxOptions()
+    options.browser_name = "firefox"
+
     def get_driver(self):
         """Initialize and return a Firefox webdriver instance."""
         driver = webdriver.Remote(
-            command_executor='http://localhost:4444/wd/hub'
+            command_executor='http://localhost:4444/wd/hub',
+            options=self.options
         )
         # driver = webdriver.Firefox()
 
